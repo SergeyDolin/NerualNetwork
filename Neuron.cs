d723a21c8067a15c394e91373d39b6dfd24f8f75
Сергей <SergeyDolin@mail.ru>
2019-09-01 00:37:16 +0700

    Add project files.

---
+++ Neuron.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NerualNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuronType NeuType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            NeuType = type;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Pow(Math.E, -x));
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
